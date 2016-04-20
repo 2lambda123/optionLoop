32f61889dae4cc94a88309ab7b618f3bf981f045
arghdos <arghdos@gmail.com>
2016-04-20 16:16:56 -0400

    auto conversion of md to rst for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+#md to rst conversion
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(name='optionloop',
 		version='1.0',
 		description='Allows collapsing of nested for loops via '
@@ -10,4 +18,5 @@ setup(name='optionloop',
 		packages=['optionloop'],
 		zip_safe=True,
 		test_suite='nose.collector',
+		long_description=read_md('README.md'),
 		tests_require=['nose'])
